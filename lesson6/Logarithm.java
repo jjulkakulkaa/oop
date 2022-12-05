5c331e9b53418c50bf18f9f9d0225386e158202f
jjulkakulkaa <114097964+jjulkakulkaa@users.noreply.github.com>
2022-12-05 12:38:09 +0100

    Update Logarithm.java

--- lesson6/Logarithm.java
+++ lesson6/Logarithm.java
@@ -10,12 +10,12 @@ public class Logarithm implements Number {
     public Logarithm(double inputBase, double inputArgument) {
       if (inputBase < 0 || inputBase == 1){
         throw new IllegalArgumentException(
-            String.format("Liczba mniejsza od 1 nie moze byc podstawa logarytmu")
+            String.format("The base of a log function cannot be equal to 1 or less than 0")
         );
       }
       if (argument < 0){
         throw new IllegalArgumentException(
-            String.format("Liczba mniejsza od o nie moze byc argumentem logarytmu")
+            String.format("The argument of a log function cannot be smaller than 0")
         );
       }
       this.base = inputBase;
@@ -27,7 +27,7 @@ public class Logarithm implements Number {
             Logarithm log = new Logarithm(0, 0);    
         } catch (IllegalArgumentException ex) {
             throw new IllegalArgumentException(
-                String.format("podane wartosci nie spelniaja wlasnosci logarytmu"),
+                String.format("Logarithm cannot be computed from given values"),
                 ex
             );
         }

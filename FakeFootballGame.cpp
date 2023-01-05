b773a3057a3c2f000cc95c6eb6e0e9652c93b0d1
jjulkakulkaa <114097964+jjulkakulkaa@users.noreply.github.com>
2023-01-05 16:24:47 +0100

    Add files via upload

---
+++ FakeFootballGame.cpp
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Game {
+public:
+    virtual string result() = 0;
+};
+
+class FakeFootballGame : public Game
+{
+private:
+    
+public:
+   string result() override {
+        return "0:15";
+   }
+};
+
+
+int main(){
+    FakeFootballGame footballGame;
+    try {
+        string score = footballGame.result();
+        if(score != "0:15"){
+            throw runtime_error("wrong score returned");
+        }
+        cout << score << endl;
+    }
+    catch(const exception& ex){
+        cout << ex.what() << endl;
+        return(1);
+    }
+    
+    return(0);
+}

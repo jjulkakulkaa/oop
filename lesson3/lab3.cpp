3f6970f882cbae080958e182e63fc5df24db5d34
jjulkakulkaa <114097964+jjulkakulkaa@users.noreply.github.com>
2023-01-26 14:46:51 +0100

    Update lab3.cpp

--- lesson3/lab3.cpp
+++ lesson3/lab3.cpp
@@ -1,51 +1,68 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Date;
+
+class Date{
+
+    string day;
+    string month;
+    string year;
+    public:
+    Date(string day, string month, string year){
+        this->day = day;
+        this->month = month;
+        this->year = year;
+    }
+    Date(int day, int month, int year){
+        if(day <10){
+            this->day = "0"+to_string(day);
+        }
+        else{
+            this->day = to_string(day);
+        }
+
+        if(month <10){
+            this->month = "0"+to_string(month);
+        }
+        else{
+            this->month = to_string(month);
+        }
+    
+        this->year = to_string(year);
+    }
+    string displayedDate(){
+        return this->day+"."+this->month+"."+this->year+"r.";
+    }
+};
 class Movie
 {
-private:
     string title;
     string director;
     string releaseDate;
+    //Date &releaseDate;
     string genre;
+
 public:
 
-    Movie(string t, string d,Date date, string g){
-        title = t;
-        director = d;
-        releaseDate = date.date_to_string();
-        genre = g;
+    Movie(string title, string director, Date &date, string genre){
+        this->title = title;
+        this->director = director;
+        this->releaseDate = date.displayedDate();
+        this->genre = genre;
 
     };
 
-    string info(){
-        return title, director, releaseDate, genre;
-    };
+    void info(){
+        cout << "Title: "+this->title+"\nDirector: "+ this->director + "\nRelease date:  "+ this->releaseDate+"\nGenre : "+this->genre;
+        };
 
 };
-class Date
-{
-    int day, month, year;
-    public:
-    Date(int d,int m,int y){
-        day = d;
-        month = m;
-        year = y;
-
-    };
-    string date_to_string()
-    {   
-        std::string d = std::to_string(day);
-        std::string m = std::to_string(month);
-        std::string y = std::to_string(year);
-        string date;
-        date = d+"."+m+"."+y+"r.";
-            return date;
-    }
-};
-
 
 int main(){
-
+    Date intDate(14,2,2018);
+    Date stringDate("14","02","2018");
+    Movie movie("Black panter","Ryan Coogler",stringDate,"sience fiction");
+    movie.info();
+    Movie sameMovie("Black panter","Ryan Coogler",intDate,"sience fiction");
+    sameMovie.info();
 }

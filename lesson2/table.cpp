121c55c7b654d3c7e9b27b181d113e36aa52a5e5
jjulkakulkaa <114097964+jjulkakulkaa@users.noreply.github.com>
2023-01-26 13:29:08 +0100

    Update table.cpp

--- lesson2/table.cpp
+++ lesson2/table.cpp
@@ -3,38 +3,30 @@ using namespace std;
 
 class Table
 {
-    public:
-
     string type_of_material;
     string colour;
     int weight;
     int price;
-    Table(string type = "no", string c = "no", int w = 0, int p = 0){
-        type_of_material = type;
-        colour = c;
-        weight = w;
-        price = p;
-    }
 
-};
-class UsedTable : public Table
-{
-    string type_of_material;
-    string colour;
-    int weight;
-    int price;
     public:
-    UsedTable(string type, string c, int w){
-    this->type_of_material=type;
-    this->colour=c;
-    this->weight=w;
+    Table(string type_of_material = "", string colour = "", int weight = 0, int price = 0){
+        this->type_of_material = type_of_material;
+        this->colour = colour;
+        this->weight = weight;
+        this->price = price;   
+    }
+    void atributes(){
+        cout<< "This table is made of "+ this->type_of_material +"\nColour : "+ this->colour +"\nWeight : " + to_string( this->weight ) + "\nPrice: "+ to_string( this->price ) + "\n";
     }
 };
+
 
 
 
 
 int main(){
     Table table("wood","black",3,3000);
+    table.atributes();
 
-}
+    Table table2("glass");
+    table2.atributes();
